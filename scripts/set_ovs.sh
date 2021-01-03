b55a6374af730260e964ecc5e7788f7e2644e731
james <greensun1231@gmail.com>
2021-01-03 11:51:07 +0000

    Add start and stop function

--- scripts/set_ovs.sh
+++ scripts/set_ovs.sh
@@ -2,19 +2,20 @@
 
 client_port=enp0s8
 ext_port=enp0s10
-client_port_num=
-ext_port_num=
+mgmt_ip_cidr=192.168.56.3/24
 mitm_ip_cidr=192.168.2.2/24
 mitm_ip=192.168.2.2
+proxy_ip=192.168.2.1
+proxy_port=3128
 #mitm_ip_16=c0a80202
 mitm_ip_16=$(printf '%02x' ${mitm_ip//./ })
 mitm_port=3128
 mitm_port_s=3129
-proxy_ip=192.168.2.1
-proxy_port=3128
+client_port_num=
+ext_port_num=
 
 get-mac() {
-  ovs_mac2=$(ovs-vsctl get interface br-ext mac-in-use | sed 's/"//g')
+  ovs_mac2=$(ip link show "$1" | grep ether | awk '{print $2}')
   echo "$ovs_mac2"
   ovs_mac1=$(echo "$ovs_mac2" | sed 's/[:"]//g')
   echo "$ovs_mac1"
@@ -26,7 +27,7 @@ get-port-num() {
 }
 
 add-flow() {
-  get-mac
+  get-mac br-ext
   get-port-num
   ovs-ofctl del-flows br0
   # 3 is br-ext port num, while 1 is port num of client <-- what if interface number changed!!!
@@ -45,21 +46,40 @@ add-flow() {
   ovs-ofctl add-flow br0 -O openflow13 priority=0,actions=normal
 }
 
-ovs-vsctl add-br br0 -- set bridge br0 other-config:hwaddr=\"08:00:27:82:a3:a8\"
-ip link set br0 up
-ip addr add 192.168.1.1/24 dev br0
+start() {
+  export PATH=$PATH:/usr/local/share/openvswitch/scripts
+  ovs-ctl start
 
-ovs-vsctl add-port br0 $client_port
-ovs-vsctl add-port br0 $ext_port
-ip link set $client_port up
-ip link set $ext_port up
+  get-mac $client_port
+  ovs-vsctl --may-exist add-br br0 -- set bridge br0 other-config:hwaddr=\"$ovs_mac2\"
+  ip link set br0 up
+  ip addr add $mgmt_ip_cidr dev br0
+  ip a del $mgmt_ip_cidr dev $client_port
 
-ovs-vsctl add-port br0 br-ext -- set interface br-ext type=internal
-macc=$(ovs-vsctl get interface br-ext mac-in-use)
-ovs-vsctl set interface br-ext mac="$macc"
-ip link set br-ext up
-ip addr add $mitm_ip_cidr dev br-ext
+  ovs-vsctl add-port br0 $client_port
+  ovs-vsctl add-port br0 $ext_port
+  ip link set $client_port up
+  ip link set $ext_port up
 
-add-flow
+  ovs-vsctl --may-exist add-port br0 br-ext -- set interface br-ext type=internal
+  macc=$(ovs-vsctl get interface br-ext mac-in-use)
+  ovs-vsctl set interface br-ext mac="$macc"
+  ip link set br-ext up
+  ip addr add $mitm_ip_cidr dev br-ext
 
-sudo python3 proxany/proxy_fwd.py 0.0.0.0 3128 $proxy_ip $proxy_port
+  add-flow
+
+  sudo python3 proxany/proxy_fwd.py 0.0.0.0 $mitm_port $proxy_ip $proxy_port &
+}
+
+stop() {
+  export PATH=$PATH:/usr/local/share/openvswitch/scripts
+  ovs-vsctl del-port $client_port # This will lose connection to client!
+  ovs-vsctl del-port $ext_port
+  ovs-ctl stop
+  ip a flush br0
+  ip a flush br-ext
+  ip a add $mgmt_ip_cidr dev $client_port
+}
+
+"$@"
